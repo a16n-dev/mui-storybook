49a53de8877aaf8676531d80d5c4648e3f3b9478
alexn400 <alexn400@gmail.com>
2021-11-05 17:38:16 +1300

    Initial commit

---
+++ .storybook/main.js
@@ -0,0 +1,17 @@
+module.exports = {
+  "stories": [
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ],
+  // This is required for now to make MUI themes work (see https://github.com/mui-org/material-ui/issues/24282#issuecomment-951015101)
+  webpackFinal(config) {
+    delete config.resolve.alias['emotion-theming'];
+    delete config.resolve.alias['@emotion/styled'];
+    delete config.resolve.alias['@emotion/core'];
+    return config;
+  },
+}
